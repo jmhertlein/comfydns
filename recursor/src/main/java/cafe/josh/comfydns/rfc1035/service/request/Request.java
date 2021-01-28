ffd2560b324eb41af35a6589c4eaaaf44092deaa
Josh Hertlein <jmhertlein@gmail.com>
2021-01-28 07:22:40 -0600

    add request time histogram

--- recursor/src/main/java/cafe/josh/comfydns/rfc1035/service/request/Request.java
+++ recursor/src/main/java/cafe/josh/comfydns/rfc1035/service/request/Request.java
@@ -2,6 +2,7 @@ package cafe.josh.comfydns.rfc1035.service.request;
 
 import cafe.josh.comfydns.rfc1035.message.struct.Message;
 import io.prometheus.client.Counter;
+import io.prometheus.client.Histogram;
 
 import java.util.UUID;
 
@@ -12,11 +13,17 @@ public abstract class Request {
     protected static final Counter requestsOut = Counter.build()
             .name("requests_out").help("All internet requests received")
             .labelNames("protocol", "rcode").register();
+    protected static final Histogram requestDurations = Histogram.build()
+            .name("request_duration").help("How long requests take, from receipt to response.")
+            .labelNames("source").register();
 
     protected final UUID id;
+    private final Histogram.Timer requestTimer;
 
     public Request() {
         id = UUID.randomUUID();
+        requestTimer = requestDurations.labels(isInternal() ? "internal" : "external")
+        .startTimer();
     }
 
     public abstract Message getMessage();
@@ -24,6 +31,7 @@ public abstract class Request {
     public abstract void answer(Message m);
 
     protected void recordAnswer(Message m, String requestProtocol) {
+        requestTimer.observeDuration();
         requestsOut.labels(requestProtocol, m.getHeader().getRCode().name().toLowerCase())
                 .inc();
     }
