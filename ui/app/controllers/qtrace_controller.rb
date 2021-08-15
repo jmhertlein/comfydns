2981f60370f5581554cb1b8f37f80ee605067a3e
Josh Hertlein <jmhertlein@gmail.com>
2021-08-15 15:07:46 -0500

    add qtrace task creation

--- ui/app/controllers/qtrace_controller.rb
+++ ui/app/controllers/qtrace_controller.rb
@@ -6,4 +6,21 @@ class QtraceController < ApplicationController
       .select{|name, value| DNS::SUPPORTED_RRTYPES.include?(name)}
       .map{|l,r| [l, l]}
   end
+
+  def qtrace
+    raw_qtype = params["qtype"]
+    unless DNS::RRTYPE_TO_VALUE.has_key? raw_qtype
+      redirect_to "/qtrace/", alert: "No such qtype: #{raw_qtype}"
+      return
+    end
+    qtype = DNS::RRTYPE_TO_VALUE[qtype]
+
+    qname = params["qname"]
+    unless /^[a-zA-Z0-9\-.]+$/.match? qname
+      redirect_to "/qtrace/", alert: "Invalid domain name: #{qname}"
+      return
+    end
+    
+    Task.create!(action: "TRACE_QUERY", args: {qname: qname, qtype: qtype})
+  end
 end
