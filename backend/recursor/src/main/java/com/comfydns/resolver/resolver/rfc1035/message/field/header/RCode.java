8989a6a448ef7f7eab739aa9c957ad0aa073ec43
Josh Hertlein <jmhertlein@gmail.com>
2021-08-19 21:09:14 -0500

    got the header ui looking ok... on desktop

--- backend/recursor/src/main/java/com/comfydns/resolver/resolver/rfc1035/message/field/header/RCode.java
+++ backend/recursor/src/main/java/com/comfydns/resolver/resolver/rfc1035/message/field/header/RCode.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public enum RCode {
     NO_ERROR(0, "No error condition"),
-    FORMAT_ERROR(1, "Format error - The name server wasunable to interpret the query."),
+    FORMAT_ERROR(1, "Format error - The name server was unable to interpret the query."),
     SERVER_FAILURE(2, "Server failure - The name server was unable to process this query due to a problem with the name server."),
     NAME_ERROR(3, "Name Error - Meaningful only for responses from an authoritative name server, this code signifies that the domain name referenced in the query does not exist."),
     NOT_IMPLEMENTED(4, "Not Implemented - The name server does not support the requested kind of query."),
