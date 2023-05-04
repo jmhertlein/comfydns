5ff0e15bdd8e37f0919cbef1bc164bcd3ff95ac6
Josh Hertlein <jmhertlein@gmail.com>
2023-05-03 20:47:04 -0500

    upgrade to rails 7 + yeet sass so it stops breaking my build >=|

--- ui/db/schema.rb
+++ ui/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_29_154544) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_014451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -19,8 +18,8 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
   create_table "ad_block_client_config", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.inet "ip", null: false
     t.boolean "block_ads", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["ip"], name: "ad_block_client_config_ip_idx"
   end
 
@@ -29,24 +28,24 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
     t.string "url", null: false
     t.boolean "auto_update", default: false, null: false
     t.string "update_frequency"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "list_type", default: "newline-list", null: false
     t.index ["url"], name: "block_list_url_key", unique: true
   end
 
   create_table "block_list_snapshot", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "block_list_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["block_list_id"], name: "block_list_snapshot_block_list_id_key", unique: true
   end
 
   create_table "blocked_name", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.uuid "block_list_snapshot_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "blocked_name_name_idx"
   end
 
@@ -55,8 +54,8 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
     t.integer "qtype"
     t.integer "qclass"
     t.integer "r_ttl"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "expires_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "expires_at", null: false
     t.text "r_name"
     t.integer "r_class"
     t.text "r_mname"
@@ -76,8 +75,8 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
     t.integer "rrclass"
     t.integer "ttl"
     t.jsonb "rdata"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "expires_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "expires_at", null: false
     t.string "original_qname"
     t.uuid "original_query_id"
     t.index ["name", "rrtype", "rrclass", "rdata"], name: "cached_rr_name_rrtype_rrclass_rdata_key", unique: true
@@ -86,8 +85,8 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
 
   create_table "flag", primary_key: "name", id: :string, force: :cascade do |t|
     t.boolean "value"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "rr", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -97,8 +96,8 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
     t.integer "ttl"
     t.jsonb "rdata"
     t.uuid "zone_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "task", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -107,8 +106,8 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
     t.boolean "done", default: false
     t.boolean "failed", default: false
     t.jsonb "args"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trace", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -116,8 +115,8 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
     t.string "qname", null: false
     t.integer "qtype", null: false
     t.integer "qclass", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trace_event", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -125,18 +124,18 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
     t.uuid "trace_id", null: false
     t.string "event_type", null: false
     t.jsonb "event", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_user_on_email", unique: true
     t.index ["reset_password_token"], name: "index_user_on_reset_password_token", unique: true
   end
@@ -144,8 +143,8 @@ ActiveRecord::Schema.define(version: 2023_04_29_154544) do
   create_table "zone", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
     t.boolean "gen_ptrs"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.uuid "soa_rr_id"
   end
 
